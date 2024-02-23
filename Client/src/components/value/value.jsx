796a0e0648ae130bda1c351c94e53b3b68a9d1c2
pavan1 <pavankumarpilaka96@gmail.com>
2024-02-23 20:17:41 +0530

    changed value.css

--- Client/src/components/value/value.jsx
+++ Client/src/components/value/value.jsx
@@ -10,7 +10,7 @@ import {
 from 'react-accessible-accordion'
 import "react-accessible-accordion/dist/fancy-example.css"
 import {MdOutlineArrowDropDown} from 'react-icons/md'
-import "./Value.css"
+import "./value.css"
 import data from "../../utils/accordion"
 const Value = () => {
   return (
